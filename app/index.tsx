350d8ef23e2ffa6b89dd2b207f558d28c68c5cba
HYUNSU <movie3342@gmail.com>
2024-10-05 13:19:13 +0900

    feat: layout, index.tsx auth 추가

--- app/index.tsx
+++ app/index.tsx
@@ -2,56 +2,51 @@ import CustomButton from "@/components/CustomButton";
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, SafeAreaView, Text, View } from "react-native";
-import {
-  GestureHandlerRootView,
-  ScrollView
-} from "react-native-gesture-handler";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+
 import { images } from "../constants";
 
 const App = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
-      <GestureHandlerRootView>
-        <ScrollView contentContainerStyle={{ height: "100%" }}>
-          <View className="w-full justify-center items-center h-full px-4">
-            <Image
-              source={images.logo}
-              className="w-[130px] h-[84px]"
-              resizeMode="contain"
-            />
-            <Image
-              source={images.cards}
-              className="max-w-[380px] w-full h-[300px]"
-              resizeMode="contain"
-            />
-            <View className="relative mt-5">
-              <Text className="text-3xl text-white font-bold text-center">
-                Discover Endless Possibilities with{" "}
-                <Text className="text-secondary-200">Aora</Text>
-              </Text>
-              <Image
-                source={images.path}
-                className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
-                resizeMode="contain"
-              />
-            </View>
-            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-              Where creativity meets innovation: embark on a journey of
-              limitless exploration with Aora
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
             </Text>
-            <CustomButton
-              title="Continue with Email"
-              onPress={() => {
-                router.push("/sign-in");
-              }}
-              containerStyles="w-full mt-7"
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
             />
           </View>
-        </ScrollView>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            onPress={() => {
+              router.push("/sign-in");
+            }}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
 
-        <StatusBar backgroundColor="#161622" style="light" />
-      </GestureHandlerRootView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };
