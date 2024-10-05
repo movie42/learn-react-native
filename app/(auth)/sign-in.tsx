37638b902c07cc052a425b051cb443d984fc46ef
HYUNSU <movie3342@gmail.com>
2024-10-05 13:17:37 +0900

    feat: signin, signup 추가

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,64 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import FormField from "@/components/FormField";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [isSubmitting, setSubmitting] = useState(false);
+  const handleSubmit = () => {
+    setSubmitting((pre) => !pre);
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            otherStyles="mt-7"
+            onChangeText={(e) => setForm({ ...form, email: e })}
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            onChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            onPress={handleSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,69 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import FormField from "@/components/FormField";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignUp = () => {
+  const [form, setForm] = useState({ email: "", password: "", username: "" });
+  const [isSubmitting, setSubmitting] = useState(false);
+  const handleSubmit = () => {
+    setSubmitting((pre) => !pre);
+  };
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold">
+            Sign Up to Aora
+          </Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            otherStyles="mt-10"
+            onChangeText={(e) => setForm({ ...form, username: e })}
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            otherStyles="mt-7"
+            onChangeText={(e) => setForm({ ...form, email: e })}
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            onChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={handleSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
